ac62815f23184e0a4d0ca5bdedd2cd6d0c6d6d92
Wevison <wevisonramalho@gmail.com>
2022-02-28 13:04:50 -0300

    [ FEAT ] Creating component product static in JS.

--- scripts/search-product.js
+++ scripts/search-product.js
@@ -29,6 +29,24 @@ function searchProduct( sunlight, water, pets ) {
                if( data.length > 0 ) {
                     results.classList.remove("hide");
                     noResults.classList.add("hide");
+
+                    const card = document.createElement("div");
+                    card.setAttribute("class", "card card-product product-staff-favorite");
+
+                    addingTagStaffFavorite( card );
+                    creatingMainCardProduct( card );       
+                    creatingFooterCardProduct( card );       
+
+                    // ✨ Staff favorite
+
+                    const elementWrapperProducts = document.getElementById("wrapper-products");
+                    elementWrapperProducts.appendChild(card);
+
+                    /*
+                    <div class="card card-product">
+                         
+                    </div>
+                    */
                }else {
                     noResults.classList.remove("hide");
                     results.classList.add("hide");
@@ -42,29 +60,143 @@ function searchProduct( sunlight, water, pets ) {
           });
 }
 
-
 function smoothScrollTo(endX, endY, duration) {
      const startX = window.scrollX || window.pageXOffset;
      const startY = window.scrollY || window.pageYOffset;
      const distanceX = endX - startX;
      const distanceY = endY - startY;
      const startTime = new Date().getTime();
- 
+
      duration = typeof duration !== 'undefined' ? duration : 400;
- 
+
      // Easing function
      const easeInOutQuart = (time, from, distance, duration) => {
        if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
        return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
      };
- 
+
      const timer = setInterval(() => {
-         const time = new Date().getTime() - startTime;
-         const newX = easeInOutQuart(time, startX, distanceX, duration);
-         const newY = easeInOutQuart(time, startY, distanceY, duration);
-         if (time >= duration) {
-         clearInterval(timer);
-         }
-         window.scroll(newX, newY);
+          const time = new Date().getTime() - startTime;
+          const newX = easeInOutQuart(time, startX, distanceX, duration);
+          const newY = easeInOutQuart(time, startY, distanceY, duration);
+          if (time >= duration) {
+               clearInterval(timer);
+          }
+          window.scroll(newX, newY);
      }, 1000 / 60); // 60 fps
- };
+};
+
+function addingTagStaffFavorite( card ) {
+     const tagFavorite = document.createElement("div");
+     tagFavorite.setAttribute("class", "tag-favorite tag-showing");
+
+     const strongFavorite = document.createElement("strong");
+     const nodeStrongFavorite = document.createTextNode("✨ Staff favorite");
+
+     strongFavorite.setAttribute("class", "text");
+     strongFavorite.appendChild( nodeStrongFavorite );
+
+     tagFavorite.appendChild( strongFavorite );
+
+     return card.appendChild( tagFavorite );
+}
+
+function creatingMainCardProduct( card ) {
+     const mainCard = document.createElement("main");
+     mainCard.setAttribute("class", "main-card");
+
+     const divContainerImage = document.createElement("div");
+     divContainerImage.setAttribute("class", "container-image");
+
+     const img = document.createElement("img");
+     img.setAttribute("class", "image");
+     img.src = "https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/ficus-lyrata.png";
+     img.alt = "Image Product.";
+
+     divContainerImage.appendChild( img );
+     mainCard.appendChild( divContainerImage );
+
+     return card.appendChild( mainCard );
+}
+
+function creatingFooterCardProduct( card ) {
+     const footerCard = document.createElement("footer");
+     footerCard.setAttribute("class", "footer-card");
+
+     const row01 = document.createElement("div");
+     row01.setAttribute("class", "row row-01");
+     
+     const col01 = document.createElement("div");
+     col01.setAttribute("class", "col col-01");
+     
+     const h3 = document.createElement("h3");
+     const nodeH3 = document.createTextNode("Bunny ears cacti");
+     h3.setAttribute("class", "title-product");
+     h3.appendChild( nodeH3 );
+
+     col01.appendChild( h3 );
+     row01.appendChild( col01 );
+     footerCard.appendChild( row01 );
+
+     const row02 = document.createElement("div");
+     row02.setAttribute("class", "row row-02");
+
+     const col01Row02 = document.createElement("div");
+     col01Row02.setAttribute("class", "col col-01");
+
+     const strong = document.createElement("strong");
+     const nodeStrong = document.createTextNode("$25");
+     strong.setAttribute("class", "text-price");
+     strong.appendChild( nodeStrong );
+
+     col01Row02.appendChild( strong );
+     row02.appendChild( col01Row02 );
+
+     const col02Row02 = document.createElement("div");
+     col02Row02.setAttribute("class", "col col-02");
+
+     const divWapperIcons = document.createElement("div");
+     divWapperIcons.setAttribute("class", "wrapper-icons");
+
+     const containerImageDog = document.createElement("div");
+     containerImageDog.setAttribute("class", "container-image container-image-dog");
+     
+     const containerImageSun = document.createElement("div");
+     containerImageSun.setAttribute("class", "container-image container-image-sun");
+     
+     const containerImageWater = document.createElement("div");
+     containerImageWater.setAttribute("class", "container-image container-image-water");
+
+     divWapperIcons.appendChild( containerImageDog );
+     divWapperIcons.appendChild( containerImageSun );
+     divWapperIcons.appendChild( containerImageWater );
+
+     col02Row02.appendChild( divWapperIcons );
+     row02.appendChild( col02Row02 );
+
+     footerCard.appendChild( row02 );
+
+     return card.appendChild( footerCard );
+
+     /*
+          <footer class="footer-card">
+               <div class="row row-02">
+                    <div class="col col-02">
+                         <div class="wrapper-icons">
+                              <div class="">
+                                   <img src="images/icon-dog.svg" class="image image-dog" alt="Icon dog." />
+                              </div>
+                              
+                              <div class="container-image container-image-sun">
+                                   <img src="images/icon-sun.svg" class="image image-sun" alt="Icon sun." />
+                              </div>
+                              
+                              <div class="container-image container-image-water">
+                                   <img src="images/icon-water.svg" class="image image-water" alt="Icon water." />
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </footer>
+     */
+}
